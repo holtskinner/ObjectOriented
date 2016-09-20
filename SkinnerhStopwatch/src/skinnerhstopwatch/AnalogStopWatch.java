43ea5e0f9cc366dd999b8bb5af759e4774c9c3ab
Holt Skinner <HoltWasHere@gmail.com>
2016-09-20 11:03:27 -0500

    Added Digital Component on top of Analog component

--- SkinnerhStopwatch/src/skinnerhstopwatch/AnalogStopWatch.java
+++ SkinnerhStopwatch/src/skinnerhstopwatch/AnalogStopWatch.java
@@ -1,6 +1,9 @@
 package skinnerhstopwatch;
 
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
 import javafx.animation.Animation;
+import javafx.animation.Animation.Status;
 import static javafx.animation.Animation.Status.RUNNING;
 import static javafx.animation.Animation.Status.STOPPED;
 import javafx.animation.KeyFrame;
@@ -9,10 +12,13 @@ import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.util.Duration;
 /**
  *
@@ -35,6 +41,9 @@ public class AnalogStopWatch {
     private Image handImage;
     private String dialImageName = "clockface.png";
     private String handImageName = "hand.png";
+    
+    private Label digitalClock;
+    private Duration timeElapsed; //Time object constructed from milliseconds
        
     
     public AnalogStopWatch() {
@@ -51,23 +60,44 @@ public class AnalogStopWatch {
         handImage = new Image(getClass().getResourceAsStream(handImageName));
         dialImageView.setImage(dialImage);
         handImageView.setImage(handImage);
-        rootContainer.getChildren().addAll(dialImageView, handImageView);       
+        
+        digitalClock = new Label("0:00.00");
+        digitalClock.setFont(new Font("Sans-Serif", 40));
+        
+        rootContainer.getChildren().addAll(dialImageView, handImageView, digitalClock);
     }
     
     
-    public void setupTimer() {
+    private void setupTimer() {
+        boolean running = false;
+        if (timeline != null) {
+            if (timeline.getStatus() == Status.RUNNING) {
+                running = true;
+                timeline.stop();
+            }
+        }
+
         keyFrame = new KeyFrame(Duration.millis(tickTimeInSeconds * 1000), (ActionEvent event) -> {
             update();
         });
         
         timeline = new Timeline(keyFrame);
-        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.setCycleCount(Animation.INDEFINITE); 
+        
+        if (running) {
+            timeline.play();
+        }
     }
     
     private void update() {
         secondsElapsed += tickTimeInSeconds;
         double rotation = secondsElapsed * angleDeltaPerSeconds;
         handImageView.setRotate(rotation);
+        
+        digitalClock.setText(String.format("%02d:%02.2f",  
+                            (int)(secondsElapsed/60),
+                            secondsElapsed % 60));
+         
     }
     
     public Parent getRootContainer() {
@@ -96,16 +126,25 @@ public class AnalogStopWatch {
         }
     }
     
-    public void reset(){
-        if (timeline.getStatus() != STOPPED){
-            stop();
-        }
-        secondsElapsed = 0.0;
+    public void reset() {
+        stop();
+        secondsElapsed = 0;
+        handImageView.setRotate(0);
+        digitalClock.setText("0:00.00");
     }
     
     public void setTickTimeInSeconds(Double tickTimeInSeconds) {
         this.tickTimeInSeconds = tickTimeInSeconds;
         setupTimer();
+    }
+    
+    public boolean isRunning() {
+        if (timeline != null) {
+            if (timeline.getStatus() == Status.RUNNING) {
+                return true;
+            }
+        } 
+        return false;
     }
     
 }
