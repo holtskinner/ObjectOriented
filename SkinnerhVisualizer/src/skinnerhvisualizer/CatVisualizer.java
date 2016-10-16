05db2f7b8af6a2866238eebbd8f59cbfee8c0703
Holt Skinner <HoltWasHere@gmail.com>
2016-10-16 15:36:56 -0500

    Added Weird rotation stuff to the cat Images

--- SkinnerhVisualizer/src/skinnerhvisualizer/CatVisualizer.java
+++ SkinnerhVisualizer/src/skinnerhvisualizer/CatVisualizer.java
@@ -5,6 +5,7 @@
  */
 package skinnerhvisualizer;
 
+import static java.lang.Integer.min;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -20,7 +21,12 @@ public class CatVisualizer implements Visualizer{
     private Integer numBands;
     private AnchorPane vizPane;
     
-    private ImageView[] catImageView;
+    private Double width = 0.0;
+    private Double height = 0.0;
+    
+    private Double bandWidth = 0.0;
+    
+    private ImageView[] catImageViews;
     private Image catImage;
     private final String catImageName = "SkinnerhCatImage.jpeg";
     
@@ -34,17 +40,54 @@ public class CatVisualizer implements Visualizer{
     }
     @Override //Create Initial UI
     public void start(Integer numBands, AnchorPane vizPane){
+        end();
         
+        this.numBands = numBands;
+        this.vizPane = vizPane;
+        this.height = vizPane.getHeight();
+        this.width = vizPane.getWidth();
+        
+        bandWidth = width / numBands;
+        
+        catImageViews = new ImageView[numBands];
+        catImage = new Image(getClass().getResourceAsStream(catImageName));
+        
+        for(int i = 0; i < numBands; i++) {
+            ImageView catImageView = new ImageView();
+            catImageView.setImage(catImage);
+            catImageView.setFitWidth(bandWidth);
+            
+            catImageView.setX(bandWidth * i);
+            catImageView.setY(i*(catImage.getHeight()/10));
+            
+            vizPane.getChildren().add(catImageView);
+            catImageViews[i] = catImageView;
+
+        }
+             
     }
     
     @Override
     public void end(){
-        
+        if(catImageViews != null) {
+            for (ImageView catImageView : catImageViews) {
+                 vizPane.getChildren().remove(catImageView);
+             }
+            catImageViews = null;
+        }
     }
     
     @Override
     public void update(double timestamp, double duration, float[] magnitudes, float[] phases){
+        if(catImageViews == null){
+            return;
+        }
         
+        Integer num = min(catImageViews.length, magnitudes.length);
+        
+        for (int i = 0; i < num; i++) {
+            catImageViews[i].setRotate((magnitudes[i]*10 -25));
+        }
     }
 }
 
