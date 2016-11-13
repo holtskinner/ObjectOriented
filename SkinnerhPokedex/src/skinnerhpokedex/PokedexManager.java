dddcaf1bb89cfd00445689504ddf797c521abbdc
Holt Skinner <HoltWasHere@gmail.com>
2016-11-13 12:21:16 -0600

    Initialized Pokedex Project

---
+++ SkinnerhPokedex/src/skinnerhpokedex/PokedexManager.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skinnerhpokedex;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import org.json.simple.*;
+
+/**
+ *
+ * @author holtskinner
+ */
+public class PokedexManager {
+    
+}
