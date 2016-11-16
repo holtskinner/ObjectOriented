8537f221e32dc3a489335310b33ab56c161d6ee2
Holt Skinner <holtwashere@gmail.com>
2016-11-16 16:33:45 -0600

    Tried to Call API didn't work
    Something is rotten in the state of Denmark

--- SkinnerhFortuneTeller/src/skinnerhfortuneteller/FortuneTellerViewController.java
+++ SkinnerhFortuneTeller/src/skinnerhfortuneteller/FortuneTellerViewController.java
@@ -1,11 +1,17 @@
 package skinnerhfortuneteller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -13,13 +19,44 @@ import javafx.scene.control.Label;
  */
 public class FortuneTellerViewController implements Initializable {
     
-    @FXML
-    private Label label;
+    String urlString = "https://helloacm.com/api/fortune/";
+    
+    String response = "";
+    
+    URL url;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button getFortune;
+    
+    @FXML
+    private TextField output;
+    
+    @FXML
+    private void getFortune(ActionEvent event) {
+        
+        try {
+            callAPI();
+        } catch (Exception e) {
+        }
+        
+        output.setText(response);
+    }
+    
+    private void callAPI() throws Exception {
+        
+        try {
+            url = new URL(urlString);
+            BufferedReader in = new BufferedReader(
+            new InputStreamReader(url.openStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                response += inputLine;
+            in.close();
+        } catch (Exception ex) {
+            throw ex;
+        }
+        
     }
     
     @Override
