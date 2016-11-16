d04028f054bb850d08a2a41e766d4f6821febba7
Holt Skinner <holtwashere@gmail.com>
2016-11-16 13:59:36 -0600

    Added Initial 2nd Screen and changed data type in Pokemon class

--- SkinnerhPokedex/src/skinnerhpokedex/Pokemon.java
+++ SkinnerhPokedex/src/skinnerhpokedex/Pokemon.java
@@ -5,12 +5,12 @@ import javafx.scene.image.Image;
 
 public class Pokemon {
     
-    private Integer number;
+    private int number;
     private String name;
     private ArrayList<String> type;
     private Image image;
 
-    public Pokemon(Integer number, String name, ArrayList<String> type, Image image) {
+    public Pokemon(int number, String name, ArrayList<String> type, Image image) {
         this.number = number;
         this.name = name;
         this.type = type;
---
+++ SkinnerhPokedex/src/skinnerhpokedex/PokemonInfoController.java
@@ -0,0 +1,22 @@
+package skinnerhpokedex;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author HS047694
+ */
+public class PokemonInfoController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
