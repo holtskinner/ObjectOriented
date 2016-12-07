aa57df2f5138e0e28a9462f00171feb259b574ed
Holt Skinner <HoltWasHere@gmail.com>
2016-12-06 18:09:20 -0600

    Got API call to work!

--- SkinnerhRelaxatron/src/skinnerhrelaxatron/EncouragementController.java
+++ SkinnerhRelaxatron/src/skinnerhrelaxatron/EncouragementController.java
@@ -1,11 +1,17 @@
 package skinnerhrelaxatron;
 
-import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import static skinnerhrelaxatron.ViewController.getControllerByName;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 
 /**
  * FXML Controller class
@@ -18,6 +24,9 @@ public class EncouragementController extends ViewController {
     Button backButton;
     
     @FXML
+    Text outputText;
+    
+    @FXML
     public void goToIndexPage() {
         ViewController.switchTo("Index");
         ViewController controller = (ViewController)getControllerByName("Index");
@@ -25,7 +34,36 @@ public class EncouragementController extends ViewController {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        outputText.setText("Message could not be found.");
+        outputText.setText(callURL("http://ec2-35-163-155-173.us-west-2.compute.amazonaws.com/api.php?fortune"));
+    }
+    
+    public static String callURL(String myURL) {
+        
+		StringBuilder sb = new StringBuilder();
+		URLConnection urlConn = null;
+		InputStreamReader in = null;
+		try {
+			URL url = new URL(myURL);
+			urlConn = url.openConnection();
+			if (urlConn != null && urlConn.getInputStream() != null) {
+				in = new InputStreamReader(urlConn.getInputStream());
+				BufferedReader bufferedReader = new BufferedReader(in);
+				int cp;
+				while ((cp = bufferedReader.read()) != -1) {
+                                    sb.append((char) cp);
+				}
+				bufferedReader.close();
+			}
+                    //I would have put this in a finally, but it can throw an exception as well
+                    if(in != null) {
+                        in.close();
+                    }
+		} catch (Exception e) {
+			throw new RuntimeException("Exception while calling URL:"+ myURL, e);
+		}
+ 
+		return sb.toString();
+	}
     
 }
