26ff756047005a0f4b097f7d5197c43b133752a3
Holt Skinner <HoltWasHere@gmail.com>
2016-12-08 20:35:39 -0600

    Formatting

--- SkinnerhRelaxatron/src/skinnerhrelaxatron/Fortune.java
+++ SkinnerhRelaxatron/src/skinnerhrelaxatron/Fortune.java
@@ -8,50 +8,55 @@ import java.net.URLConnection;
 
 public class Fortune {
 
-  private String text;
-  private static String url ="http://ec2-35-163-155-173.us-west-2.compute.amazonaws.com/api.php?fortune";
+    private String text;
+    private static final String urlString ="http://ec2-35-163-155-173.us-west-2.compute.amazonaws.com/api.php?fortune";
 
-  public Fortune() throws IOException {
-    this.text = callURL(url);
-  }
+    public Fortune() throws IOException {
+        try {
+            this.text = callURL(urlString);
+        } catch (Exception ex) {
+            System.out.println(ex);
+            this.text = "";
+        }
+    }
 
-  public String getText() {
-    return this.text;
-  }
+    public String getText() {
+      return this.text;
+    }
 
-  private static String callURL(String urlString) throws IOException {
+    private static String callURL(String urlString) throws IOException {
 
-          URLConnection urlConn = null; //Assign it to null, just to be safe
-          InputStreamReader input = null;
-          String output = null;
-          BufferedReader bufferedReader = null;
+        URLConnection urlConn = null; //Assign it to null, just to be safe
+        InputStreamReader input = null;
+        String output = null;
+        BufferedReader bufferedReader = null;
 
-          try {
-              URL url = new URL(urlString);
-              urlConn = url.openConnection();
-              if (urlConn != null && urlConn.getInputStream() != null) {
+        try {
+            URL url = new URL(urlString);
+            urlConn = url.openConnection();
+            if (urlConn != null && urlConn.getInputStream() != null) {
 
-                  input = new InputStreamReader(urlConn.getInputStream());
-                  bufferedReader = new BufferedReader(input);
-                  output = bufferedReader.readLine();
+                input = new InputStreamReader(urlConn.getInputStream());
+                bufferedReader = new BufferedReader(input);
+                output = bufferedReader.readLine();
 
-                  if (output == null) {
-                      output = "";
-                  }
+                if (output == null) {
+                    output = "";
+                }
 
-                  bufferedReader.close();
-              }
+                bufferedReader.close();
+            }
 
-              //I would have put this in a finally, but it can throw an exception as well
-              if(input != null) {
-                  input.close();
-              }
+            //I would have put this in a finally, but it can throw an exception as well
+            if(input != null) {
+                input.close();
+            }
 
-          } catch (IOException ex) {
-                  System.out.println(ex);
-          }
+        } catch (IOException ex) {
+                System.out.println(ex);
+        }
 
-          return output;
-}
+        return output;
+    }
 
 }
